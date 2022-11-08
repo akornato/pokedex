36e9e9d0beb38a9df757e76890cfeb6db0ba82f4
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-11-08 11:20:02 +0100

    simplify inheritance

--- sol/contracts/Pokemon.sol
+++ sol/contracts/Pokemon.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Pokemon is ERC721, ERC721URIStorage, Ownable {
+contract Pokemon is ERC721URIStorage, Ownable {
     /**
      * @dev Emitted when `tokenId` is minted
      */
@@ -33,17 +32,14 @@ contract Pokemon is ERC721, ERC721URIStorage, Ownable {
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
         super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721URIStorage)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
